f0869c7557d189377ff90b910ab0f5da1e6ea162
SaisindhuJ <sj23855n@pace.edu>
2023-04-07 11:33:55 -0400

    Images added

--- screens/BottomNavigator/Notifications.jsx
+++ screens/BottomNavigator/Notifications.jsx
@@ -1,11 +1,11 @@
 import React, { useState } from 'react'
-import { TouchableOpacity, StyleSheet, View } from 'react-native'
+import { TouchableOpacity, StyleSheet, View,Image } from 'react-native'
 import Background from '../../components/Background'
 
 const Notifications = () => {
   return (
     <Background>
-      
+      <Image source={require('../../images/Notifications.png')} className="w-64 h-64 object-contain"/>
     </Background>
   )
 }
